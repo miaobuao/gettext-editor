4ef43049a184fd13697fbba2ae32a6c894e7cb6b
yangqiuyi <yangqiuyi@yaocheng.cn>
2023-11-29 19:17:11 +0800

    settings

--- src/pages/editor/settings.vue
+++ src/pages/editor/settings.vue
@@ -1,1 +1,12 @@
-<template>settings</template>
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>{{ $t('common.settings') }}</v-card-title>
+      <v-card-item></v-card-item>
+      <v-card-actions>
+        <v-btn>{{ $t('common.save') }}</v-btn>
+        <v-btn>{{ $t('common.reset') }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
